5b631ffa6626673b089829da2c647a0df984c549
rairishabharun-afk <rai.rishabh.arun@gmail.com>
2026-06-18 18:16:52 +0530

    update

--- libraries/Seaborn1/AIML1/basic.py
+++ libraries/Seaborn1/AIML1/basic.py
@@ -59,15 +59,34 @@
 # print(f'Study-Marks correlation: r={r:.3f}, p={p_value:.4f}')
 # print('Interpretation:', 'strong positive' if r>0.7 else 'Moderate' if r>0.4 else 'Weak')
 
+
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from scipy.stats import norm 
+
+# mean_h, std_h = 165, 7
+
+# prob = 1 - norm.cdf(175, mean_h, std_h)
+# print(f'P(heigh > 175cm) = {prob:.4f} = {prob*100:.1f}%')
+
+# print(f'68% of people: {mean_h-std_h:.0f}cm to {mean_h+std_h:.0f}cm')
+# print(f'95% of people: {mean_h-2*std_h:.0f}cm to {mean_h+2*std_h:.0f}cm')
+# print(f'99.7% of people: {mean_h-3*std_h:.0f}cm to {mean_h+3*std_h:.0f}cm')
+
+
+from sklearn.model_selection import train_test_split, cross_val_score
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.stats import norm 
+np.random.seed(42)
+x = np.random.randn(500, 5) 
+y = np.random.randint(0,2,500)
 
-mean_h, std_h = 165, 7
+X_train,X_test,y_train,y_test = train_test_split(
+    x, y, test_size=0.2, random_state=42, stratify=y
+)
+print(f'Training samples: {len(X_train)} | Test samples: {len(X_test)}')
 
-prob = 1 - norm.cdf(175, mean_h, std_h)
-print(f'P(heigh > 175cm) = {prob:.4f} = {prob*100:.1f}%')
-
-print(f'68% of people: {mean_h-std_h:.0f}cm to {mean_h+std_h:.0f}cm')
-print(f'95% of people: {mean_h-2*std_h:.0f}cm to {mean_h+2*std_h:.0f}cm')
-print(f'99.7% of people: {mean_h-3*std_h:.0f}cm to {mean_h+3*std_h:.0f}cm')
+from sklearn.ensemble import RandomForestClassifier
+model = RandomForestClassifier(n_estimators=50, random_state=42)
+cv_scores = cross_val_score(model,x,y, cv=5, scoring='accuracy')
+print('f CV Scores each fold: {cv_scores.round(3)}')
+print(f'Mean: {cv_scores.mean():.4f} +_ {cv_scores.std():.4f}')
